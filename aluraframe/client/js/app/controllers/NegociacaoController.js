ffd741423ce91000b4c23dec8052ab298c8bf852
Mateus Guedes <teuguedes@outlook.com>
2019-05-02 05:26:17 -0300

    metodo apagar implementado

--- aluraframe/client/js/app/controllers/NegociacaoController.js
+++ aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -6,7 +6,9 @@ class NegociacaoController {
     this._inputData = $('#data');
     this._inputQuantidade = $('#quantidade');
     this._inputValor = $('#valor');
-    this._listaNegociacoes = new ListaNegociacoes()
+    this._listaNegociacoes = new ListaNegociacoes(model => {
+      this._negociacoesView.update(model)
+    })
     
     this._negociacoesView = new NegociacoesView($("#negociacoesView"))
     this._negociacoesView.update(this._listaNegociacoes)
@@ -14,18 +16,25 @@ class NegociacaoController {
     this._mensagem = new Mensagem()
     this._mensagemView = new MensagemView($("#mensagemView"))
     this._mensagemView.update(this._mensagem)
+
+
   }
 
   adiciona(event) {
     event.preventDefault();
 
     this._listaNegociacoes.adiciona(this._criaNegociacao())
-    this._negociacoesView.update(this._listaNegociacoes)
 
     this._mensagem.texto = 'Negociação adicionada com sucesso'
     this._mensagemView.update(this._mensagem)
 
     this.limparCampos()
+  }
+
+  apaga() {
+    this._listaNegociacoes.esvazia();
+    this._mensagem.texto = 'Negociações apagadas com sucesso'
+    this._mensagemView.update(this._mensagem)
   }
 
   _criaNegociacao() {
